1df821365f15df4c355bf664858186edac0c2c2c
Thorsten Lorenz <thlorenz@gmx.de>
2013-01-27 18:30:01 -0500

    working cursor jump
    - cursor jumps back to specified position
    - jumps to original pos either on timeout or if another key is pressed

--- readline-matchparens.js
+++ readline-matchparens.js
@@ -3,10 +3,28 @@
 var logln = require('./lib/utl').logln;
 
 var override = module.exports = function override_ttyWrite(rli) {
-  var original_ttyWrite =  rli._ttyWrite;
+  var original_ttyWrite =  rli._ttyWrite
+    , pendingMoveBack;
+
+  function moveCursorBack() {
+    if (pendingMoveBack) rli._moveCursor(pendingMoveBack);
+    pendingMoveBack = 0;
+  } 
+
   rli._ttyWrite = function () {
+    var matchAt, move;
+
+    moveCursorBack();
     original_ttyWrite.apply(rli, arguments);
 
-    logln(rli.line);
+    matchAt = Math.max(0, rli.cursor - 3);
+    move = matchAt - rli.cursor;
+    pendingMoveBack = -move;
+    rli._moveCursor(move);
+
+    setTimeout(moveCursorBack, 200);
+
+
+    logln({ line: rli.line, cursor: rli.cursor });
   };
 };
