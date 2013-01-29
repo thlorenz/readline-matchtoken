2787b5454f893494787dab86c56cdf143d5b1322
Thorsten Lorenz <thlorenz@gmx.de>
2013-01-29 07:59:42 -0500

    testing match close bracket

--- test/readline-matchtoken.js
+++ test/readline-matchtoken.js
@@ -6,21 +6,24 @@ var readlineMatchToken = require('..')
   , harness = require('readline-testharness')
   , createHarness = harness.create
   , hns
+  , cursorAfter
+  , cursorBefore
 
 function createrli() {
   var rli = harness.rli()
   rli.line = ''
   rli._ttyWrite = function (code, key) { 
-    this.line += key.name 
+    if (!cursorBefore) this.line += key.name 
+    else this.line = this.line.slice(0, cursorBefore) + key.name + this.line.slice(cursorBefore)
 
     // move cursor behind entered char
-    this.cursor = this.line.length;
-
+    this.cursor = cursorAfter || this.line.length;
   }
   return rli;
 }
 
 function setup() {
+  cursorAfter = cursorBefore = undefined
   readlineMatchToken.interval = 10
   hns = createHarness(readlineMatchToken, createrli)
   hns.reset()
@@ -58,3 +61,22 @@ test('given rli line "(.." and I enter ")" and enter a [space] right after', fun
   t.equal(hns.rli.moveCursor.pop(), 4, 'moves cursor back as [space] is entered')
 });
 
+test('given rli line "function foo () { console.log("hello world"); " and I enter "}"', function (t) {
+  setup()
+
+  hns.rli.line = 'function foo () { console.log("hello world"); '
+  hns.key('}')
+  t.equal(hns.rli.moveCursor.pop(), -31, 'moves cursor onto matching brace')
+});
+
+test('given rli line "function foo () console.log("hello world"); }", the cursor is before "console" and I enter "{"', function (t) {
+  setup()
+  
+  //              0123456789012345
+  hns.rli.line = 'function foo () console.log("hello world"); }'
+
+  cursorBefore = 15
+  cursorAfter = 16
+  hns.key('{')
+  t.equal(hns.rli.moveCursor.pop(), 29, 'moves cursor onto matching close brace')
+});
